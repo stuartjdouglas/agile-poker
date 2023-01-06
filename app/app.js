b6c3e0a62e63ae2df6054e3fe7593435d229ab2a
Stuart Douglas <stuartjdouglas@icloud.com>
2023-01-06 17:23:49 +0000

    Now holds history of votes,

--- app/app.js
+++ app/app.js
@@ -1,87 +1,107 @@
 const { createApp } = Vue
-    const socket = io();
+const socket = io();
 
-    createApp({
-        data() {
-            return {
-                votes: [0, 1, 2, 3, 5, 8, 13, 20, 40, '?'],
-                name: null,
-                game: {
-                    concluded: false,
-                    votes: []
-                }
+createApp({
+    data() {
+        return {
+            cards: [0, 1, 2, 3, 5, 8, 13, 20, 40, '?'],
+            hasName: false,
+            name: null,
+            socketID: null,
+            votes: null,
+            vote: null,
+            connectedVoters: null
+        }
+    },
+    methods: {
+        issueVote: function (voteValue) {
+            if (this.name) {
+                socket.emit('vote', {
+                    id: this.socketID,
+                    username: this.name,
+                    vote: voteValue
+                });
             }
         },
-        methods: {
-            issueVote: function (voteValue) {
-                if (this.name) {
-                    socket.emit('vote', {
-                        username: this.name,
-                        vote: voteValue
-                    });
-                }
-            },
-            newVote: function() {
-                if (this.game.concluded) {
-                    socket.emit('new vote')
-                }
-            },
-            revealVotes: function() {
-                if (!this.game.concluded) {
-                    socket.emit('reveal votes')
-                }
-            },
-            setName: function () {
-                if (this.name && !this.game.concluded) {
-                    socket.emit('hi', this.name);
-                    this.changeName = false;
-                } else {
-                    console.log('please set your name')
-                }
-            },
-            getCardClass: function (vote) {
-                const user = this.game?.votes?.find(vote => vote?.username === this.name);
-                return { voted: user?.vote === vote };
-            },
-            clearName: function () {
-                localStorage.removeItem('name');
-                this.name = undefined;
+        newVote: function (name) {
+            if (!this.vote || this.vote.concluded) {
+                console.log('Creating new vote');
+                socket.emit('new vote', name);
             }
         },
-        mounted() {
-            if (localStorage.name) {
-                this.name = localStorage.name
+        revealVotes: function () {
+            if (!this.vote.concluded) {
+                socket.emit('reveal votes')
             }
         },
-        created() {
-            socket.on('voted', (game) => {
-                this.game = game;
-                console.log('game', game);
+        setName: function () {
+            if (this.name) {
+                socket.emit('hi', this.name);
+                this.changeName = false;
+                this.hasName = true;
+            } else {
+                console.log('please set your name')
+            }
+        },
+        getCardClass: function (vote) {
+            const user = this.vote?.votes?.find(vote => vote?.username === this.name);
+            return { voted: user?.vote === vote };
+        },
+        clearName: function () {
+            localStorage.removeItem('name');
+            this.name = undefined;
+        },
+        hasSomeoneVoted: function () {
+            return this.vote && this.vote.votes.find(vote => vote.vote !== undefined);
+        },
+        getHistory: function () {
+            return this.votes.sort((a, b) => {
+                return new Date(b.dateCreated) - new Date(a.dateCreated);
             });
+        }
+    },
+    mounted() {
+        if (localStorage.name) {
+            this.name = localStorage.name
+            this.hasName = true;
+        }
+    },
+    created() {
+        const getCurrentVote = (votes) => votes[votes.length - 1];
 
-            socket.on('reveal votes', (game) => {
-                console.log('reveal the votes');
-                this.game = game;
-            });
+        socket.on('voted', (votes) => {
+            console.log('Voted');
+            this.vote = getCurrentVote(votes.votes);
+            this.votes = votes.votes;
+            this.connectedVoters = votes.connectedVoters;
+        });
 
-            socket.on('new vote', (game) => {
-                console.log('new vote started');
-                this.game = game;
-                if (localStorage.name) {
-                    socket.emit('hi', localStorage.name);
-                }
-            });
-            
+        socket.on('reveal votes', (votes) => {
+            console.log('Votes revealed');
+            this.vote = getCurrentVote(votes.votes);
+            this.votes = votes.votes;
+            this.connectedVoters = votes.connectedVoters;
+        });
+
+        socket.on('new vote', (votes) => {
+            console.log('New Vote Created');
+            this.vote = getCurrentVote(votes.votes);
+            this.votes = votes.votes;
+            this.connectedVoters = votes.connectedVoters;
             if (localStorage.name) {
                 socket.emit('hi', localStorage.name);
             }
-        },
-        watch: {
-            name(newName) {
-                localStorage.name = newName;
-            },
-            game(gameUpdate) {
-                localStorage.game = gameUpdate;
-            }
+        });
+        if (localStorage.name) {
+            socket.emit('hi', localStorage.name);
         }
-    }).mount('#app')
+    },
+    watch: {
+        name(newName) {
+            localStorage.name = newName;
+        },
+        game(gameUpdate) {
+            localStorage.game = gameUpdate;
+        }
+    }
+}).mount('#app')
--- index.js
+++ index.js
@@ -6,25 +6,42 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const port = process.env.PORT || 3000;
 
+/**
+ * User vote class.
+ */
 class UserVote {
+
+    /** User id */
     id;
 
+    /** Username */
     username;
 
+    /** The user vote */
     vote;
+
+    /** Flag to mark user as moderator */
+    isModerator;
 
     constructor(username, vote) {
         this.username = username;
         this.id = username;
         this.vote = vote;
+        this.isModerator;
     }
 }
 
+
+/** Result class for a vote */
 class Result {
+
+    /** The vote */
     vote;
 
+    /** The number of votes */
     numOfVotes;
 
+    /** The list of voters */
     voters;
 
     constructor(vote, numOfVotes, voters) {
@@ -34,23 +51,59 @@ class Result {
     }
 }
 
+/** Vote */
 class Vote {
+
+    /** Name of the vote */
+    name;
+
+    /** List of user votes */
     votes;
 
+    /** Flag to mark vote as concluded */
     concluded;
 
+    /** The results */
     results;
 
-    constructor() {
+    /** The date time of creation */
+    dateCreated;
+
+    /** The date time of conclusion */
+    dateConcluded;
+
+    constructor(name) {
+        this.name = name;
+        this.dateCreated = new Date();
         this.votes = [];
         this.concluded = false;
         this.results = [];
     }
 }
 
-let game = new Vote();
+/** Game */
+class Game {
+    /** votes */
+    votes;
 
+    /** The connected players */
+    connectedVoters;
 
+    constructor() {
+        this.votes = [];
+        this.connectedVoters = [];
+    }
+
+    /** @returns the current vote in place */
+    currentVote() {
+        return this.votes[this.votes.length - 1];
+    }
+}
+
+// Create game instance
+const game = new Game()
+
+// Host content
 app.use(express.static('app'));
 
 app.get('/', (req, res) => {
@@ -58,71 +111,110 @@ app.get('/', (req, res) => {
 });
 
 
-
+// Socket IO communication
 io.on('connection', (socket) => {
+
+    /**
+     * Handles revealing the vote
+     */
     function handleRevealResults() {
-        console.log('Reavealing the votes');
-        game.concluded = true;
-    
-        game.votes.forEach(vote => {
-            const result = game.results.find(result => result.vote === vote.vote);
+        console.log('Revealing the votes');
+        const vote = game.currentVote();
+        vote.concluded = true;
+
+        vote.votes.forEach(userVote => {
             console.log(vote);
-            if (!result) {
-                game.results.push(new Result(vote.vote, 1, [vote.id]));
-            } else {
-                result.numOfVotes = result.numOfVotes + 1;
-                result.voters.push(vote.id);
+            const result = vote.results.find(result => result.vote === userVote.vote);
+
+            if (userVote.vote) {
+                if (!result) {
+                    vote.results.push(new Result(userVote.vote, 1, [userVote.id]));
+                } else {
+                    result.numOfVotes = result.numOfVotes + 1;
+                    result.voters.push(userVote.id);
+                }
             }
         });
-    
-        console.table(game.results);
+
+        console.table(vote.results);
         broadcastUpdate('reveal votes');
     }
-    
-    function handleNewVote() {
-        console.log('New vote started');
-        game.votes = [];
-        game.results = [];
-        game.concluded = false;
+
+    /**
+     * Handles creating a new vote
+     * @param name the vote name 
+     */
+    function handleNewVote(name) {
+        const vote = new Vote(name);
+        console.log('New vote started', name);
+        if (!game.votes) {
+            game.votes = new Array(vote);
+        } else {
+            game.votes.push(vote);
+        }
         broadcastUpdate('new vote');
     }
-    
+
+    /**
+     * Handles clients communicating.
+     *
+     * @param username the username 
+     */
     function handleHi(username) {
-        console.log(`User ${username} says hi`)
-        console.log(game);
+        console.log(`User ${username} says hi`);
+        if (!game.connectedVoters.includes(username)) {
+            game.connectedVoters.push(username);
+        }
         const userVote = getUsersVote(username);
         if (userVote) {
             userVote.vote = null;
         } else {
-            game.votes.push(new UserVote(username));
+            if (game.currentVote()) {
+                game.currentVote().votes.push(new UserVote(username));
+            }
         }
         broadcastUpdate('voted');
     }
-    
+
+    /**
+     * Handles getting a vote from the client.
+     *
+     * @param action data from client
+     */
     function handleVote(action) {
-            console.log(`User ${action.username} has voted ${action.vote}`);
-            if (!game.concluded) {
-                const user = getUsersVote(action.username);
-                if (user) {
-                    user.vote = action.vote;
-                } else {
-                    game.votes.push(new UserVote(action.username, action.vote));
-                }
-                broadcastUpdate('voted');
+        const vote = game.currentVote();
+        console.log(`User ${action.username} has voted ${action.vote}`);
+        if (vote && !vote.concluded) {
+            const user = getUsersVote(action.username);
+            if (user) {
+                user.vote = action.vote;
+            } else {
+                vote.votes.push(new UserVote(action.username, action.vote));
             }
+            broadcastUpdate('voted');
+        }
     }
-    
+
+    /**
+     * Returns the users vote.
+     * @param userId the user id
+     * @returns the users vote
+     */
     function getUsersVote(userId) {
-        if (!game.votes) {
+        const vote = game.currentVote();
+        if (!vote || vote && !vote.votes) {
             return;
         }
-        return game?.votes?.find(vote => vote.id === userId);
+        return vote.votes.find(vote => vote.id === userId);
     }
-    
+
+    /** Broadcasts updates to all users */
     function broadcastUpdate(subject) {
         socket.broadcast.emit(subject, game);
         socket.emit(subject, game);
     }
+
+
     console.log('A user has connected');
     broadcastUpdate('voted');
 
